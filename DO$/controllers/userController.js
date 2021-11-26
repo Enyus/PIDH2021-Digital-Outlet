c795672b8880b26ae2a9c155271e3f966de45953
Flavio <Flavio@aeis-mazza.com>
2021-11-26 13:30:45 -0300

    Incluído o controller 'carrinho' aos controllers

--- DO$/controllers/userController.js
+++ DO$/controllers/userController.js
@@ -2,5 +2,6 @@ module.exports = {
     index: (req, res) => res.render('paginacliente', {title:"Bem-Vindo!"}),
     login: (req, res) => res.render('login', {title:"Bem-Vindo!"}),
     cadastro: (req, res) => res.render('cadastrousuario', {title:"Seja nosso Cliente!"}),
-    cadastroLoja: (req, res) => res.render('cadastroloja', {title:"Seja nosso Parceiro!"})
+    cadastroLoja: (req, res) => res.render('cadastroloja', {title:"Seja nosso Parceiro!"}),
+    carrinho: (req, res) => res.render('carrinho', {title:"Carrinho!"})
 }
