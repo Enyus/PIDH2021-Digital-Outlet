8187a01a7233fed55483a8e84eca40fe6de556db
agabitoledo <gabriellefariat@gmail.com>
2021-12-02 19:22:56 -0300

    Carrinho-identificação com rotas criadas e principais elementos.

--- DO$/controllers/CompraController.js
+++ DO$/controllers/CompraController.js
@@ -1,10 +1,12 @@
 module.exports = {
     index: (req, res, next) => {
         res.render('carrinho-sacola', {title: "Sacola"});
-
     },
     sacola: (req, res, next) => {
         res.render('carrinho-sacola', {title: "Sacola"});
+    },
+    identificacao: (req, res, next) => {
+        res.render('carrinho-identificacao', {title: "Identificacao"})
     },
     entrega: (req, res, next) => {
         res.render('carrinho-entrega', {title: "Entrega"})
