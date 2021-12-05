dae01891b6580f1f5d9c988e62a031fb75f74a5d
Flavio <Flavio@aeis-mazza.com>
2021-12-05 10:47:45 -0300

    Desenvolvendo a página 'carrinho-pagamento'

--- DO$/controllers/CompraController.js
+++ DO$/controllers/CompraController.js
@@ -1,12 +1,14 @@
 module.exports = {
     index: (req, res, next) => {
         res.render('carrinho-sacola', {title: "Sacola"});
-
     },
     sacola: (req, res, next) => {
         res.render('carrinho-sacola', {title: "Sacola"});
     },
     entrega: (req, res, next) => {
         res.render('carrinho-entrega', {title: "Entrega"})
+    },
+    pagamento: (req, res, next) => {
+        res.render('carrinho-pagamento', {title: "Pagamento"})
     }
 }
--- DO$/routes/compra.js
+++ DO$/routes/compra.js
@@ -5,5 +5,6 @@ const CompraController = require('../controllers/CompraController')
 router.get('/', CompraController.index)
 router.get('/sacola', CompraController.sacola)
 router.get('/entrega', CompraController.entrega)
+router.get('/pagamento', CompraController.pagamento)
 
 module.exports = router
