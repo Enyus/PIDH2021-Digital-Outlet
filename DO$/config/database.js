20c0748f4dd69d262cbb8740da467acb3d7c4ebf
FlavioFLV <flavioprado78@gmail.com>
2022-01-27 22:04:38 -0300

    Criando banco de dados

---
+++ DO$/config/database.js
@@ -0,0 +1,9 @@
+const config = {
+  username: 'root',
+  password: 'flv1503',
+  database: 'dho_db',
+  host: 'localhost',
+  dialect: 'mysql'
+}
+
+module.exports = config;
---
+++ DO$/models/usuarios.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataType) => {
+	const Usuarios = sequelize.define('Usuarios', {
+	    idUsuario: {
+	      type: DataType.INTEGER,
+	      primaryKey: true,
+	      autoIncrement: true
+	    },
+    	email: DataType.STRING,
+	    nome: DataType.STRING,
+	    sobrenome: DataType.STRING,
+	    dataNasc: DataType.DATE,
+        cpf: DataType.INTEGER(11),
+        senha: DataType.STRING,
+        fotoPerfil: DataType.STRING
+	},{
+	   tableName: 'Usuarios',
+	});
+	return Usuarios;
+};
