26a50787894c1b745eab13c0fdaf1160c1816505
Enyus <enyus@hotmail.com>
2022-03-03 11:11:11 -0300

    Atualizando Seeders para produtos das categorias Acessórios, Esporte e Livros

--- database/seeders/20220222115300-demo-categorias.js
+++ database/seeders/20220222115300-demo-categorias.js
@@ -2,31 +2,38 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Categorias', [{
+    await queryInterface.bulkInsert('Categorias', [
+    //idCategoria=1
+      {
       nomeCategoria: 'Roupas',
       createdAt: new Date(),
       updatedAt: new Date()
     },
+    //idCategoria=2
     {
       nomeCategoria: 'Livros',
       createdAt: new Date(),
       updatedAt: new Date()
     },
+    //idCategoria=3
     {
       nomeCategoria: 'Calçados',
       createdAt: new Date(),
       updatedAt: new Date()
     },
+    //idCategoria=4
     {
       nomeCategoria: 'Acessórios',
       createdAt: new Date(),
       updatedAt: new Date()
     },
+    //idCategoria=5
     {
       nomeCategoria: 'Para a Casa',
       createdAt: new Date(),
       updatedAt: new Date()
     },
+    //idCategoria=6
     {
       nomeCategoria: 'Esporte',
       createdAt: new Date(),
