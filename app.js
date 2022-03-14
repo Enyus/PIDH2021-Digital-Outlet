0d5a76ffbdfde273c40c6b9d2413afec5826f61f
Enyus <enyus@hotmail.com>
2022-03-14 16:16:23 -0300

    fazendo o PUT de alteração dos dados do cliente

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var logger = require('morgan');
+const methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 // app.use('/usuario', usersRouter);
