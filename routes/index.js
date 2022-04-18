6cd8017eba14143e45cd4f6c8c3c1d16c449e2ea
FlavioFLV <flavioprado78@gmail.com>
2022-04-17 23:50:32 -0300

    Criado rotas 'create' 'update'

--- routes/index.js
+++ routes/index.js
@@ -52,8 +52,10 @@ router.get('/trabalheconosco', IndexController.trabalheconosco)
 router.post('/trabalheconosco', uploadFile.single('curriculo'), IndexController.cadastrarCurriculo)
 
 /*GET Página do Cadastro de Produto*/
-router.get('/cadastroproduto', IndexController.cadastroproduto);
-router.post('/cadastroproduto', authLoja, uploadFotoProduto.array('gallery', 6), ProdutoController.create);
+router.get('/cadastroproduto', authLoja, IndexController.cadastroproduto);
+router.post('/cadastroproduto', uploadFotoProduto.array('gallery', 6), ProdutoController.create);
+router.get('/editarProduto/:idProduto', authLoja, ProdutoController.showProduto);
+router.put('/editarProduto', authLoja, uploadFotoProduto.array('gallery', 6), ProdutoController.update);
 
 /*GET Página do Cliente*/
 router.get('/cliente', auth, userController.paginacliente);
